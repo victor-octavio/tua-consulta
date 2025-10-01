0dafa50a617fe391542397ab5875b24aff30a8a7
Victor Octavio <victoroctavio4@gmail.com>
2025-10-01 18:16:48 -0300

    Add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '**',
+        redirectTo: 'login',
+    },
+    {
+        path: 'login',
+        component: Login
+    }
+];
